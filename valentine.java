47d81644db6be37ad868b3023e8e01af55b00ba6
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-13 11:49:13 +0530

    Will you be my valentine?

---
+++ valentine.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class valentine {
+    public static void main (String agrs[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Will you be my valentine?");
+        String response = sc.next();
+        if (response.equals("yes")){
+            System.out.println("Yay Happy valentines day <3");
+        }else{
+                System.out.println("Invalid answer");
+            }
+        }
+         
+        }
+
+    
+    
+
